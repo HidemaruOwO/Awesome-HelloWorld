6e2100297c3e052c932d531d63373a8deaa7d831
うぃーうぉん <80945906+MWindows99@users.noreply.github.com>
2023-03-02 00:42:54 +0900

    レスポンス返却後に自主的に終了するように変更 (#45)

--- python/hello_world_6/main.py
+++ python/hello_world_6/main.py
@@ -1,12 +1,24 @@
 #!/usr/bin/env python3
 
+import os
+import time
+import threading
+import psutil
 import uvicorn
 from fastapi import FastAPI
 
 app = FastAPI(docs_url=None, redoc_url=None, openapi_url=None)
 
+def killer():
+    time.sleep(1)
+    p = psutil.Process(os.getpid())
+    p.kill()
+    
 @app.get("/")
 async def hello():
+    t = threading.Thread(target=killer)
+    t.start()
+    print("Hello World!!")
     return "Hello World!!"
 
 uvicorn.run(app, host="127.0.0.1", port=8000)
