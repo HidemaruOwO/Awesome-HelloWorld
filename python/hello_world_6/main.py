6d5be99dbabbc581000855ecdfbbad5e13b117ba
うぃーうぉん <80945906+MWindows99@users.noreply.github.com>
2023-03-02 01:02:28 +0900

    GitHub Actionsのエラー回避

--- python/hello_world_6/main.py
+++ python/hello_world_6/main.py
@@ -10,15 +10,15 @@ from fastapi import FastAPI
 app = FastAPI(docs_url=None, redoc_url=None, openapi_url=None)
 
 def killer():
-    time.sleep(1)
+    time.sleep(10)
     p = psutil.Process(os.getpid())
     p.kill()
-    
+
 @app.get("/")
 async def hello():
-    t = threading.Thread(target=killer)
-    t.start()
     print("Hello World!!")
     return "Hello World!!"
 
+t = threading.Thread(target=killer)
+t.start()
 uvicorn.run(app, host="127.0.0.1", port=8000)
