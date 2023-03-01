aa4099742814b405b5611911b42fbd0fce7a7b8e
うぃーうぉん <80945906+MWindows99@users.noreply.github.com>
2023-03-02 01:17:23 +0900

    GitHub Actionsのエラー回避 (#50) close #45
    Co-authored-by: Syuugo <pub@s1204.me>

--- python/hello_world_6/main.py
+++ python/hello_world_6/main.py
@@ -3,6 +3,7 @@
 import os
 import time
 import threading
+import requests
 import psutil
 import uvicorn
 from fastapi import FastAPI
@@ -13,7 +14,11 @@ def killer():
     time.sleep(1)
     p = psutil.Process(os.getpid())
     p.kill()
-    
+
+def auto_access():
+    time.sleep(10)
+    requests.get("http://127.0.0.1:8000")
+
 @app.get("/")
 async def hello():
     t = threading.Thread(target=killer)
@@ -21,4 +26,6 @@ async def hello():
     print("Hello World!!")
     return "Hello World!!"
 
+t = threading.Thread(target=auto_access)
+t.start()
 uvicorn.run(app, host="127.0.0.1", port=8000)
