7b2cddf0784b480f69e017a61163583b823f03d5
yabeenico <>
2023-03-02 02:14:29 +0900

    while

--- kubernetes/hello_world_1/main.sh
+++ kubernetes/hello_world_1/main.sh
@@ -1,3 +1,3 @@
 kubectl run --restart=Never --image=hello-world my-hello-world
-sleep 3
+while ! kubectl get pods my-hello-world 2>/dev/null | grep -q Completed; do sleep 1; done
 kubectl logs my-hello-world
