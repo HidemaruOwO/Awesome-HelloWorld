ed330faac8bb83ab3ab22e3e35a84d18b1315f3a
shiumano <takkunmail2005@gmail.com>
2023-03-01 12:16:50 +0900

    リテラル禁止縛り

---
+++ python/hello_world_3/main.py
@@ -0,0 +1,46 @@
+ONE   = int(bool(bool))
+TWO   = ONE   + ONE
+THREE = TWO   + ONE
+FOUR  = THREE + ONE
+FIVE  = FOUR  + ONE
+SIX   = FIVE  + ONE
+SEVEN = SIX   + ONE
+EIGHT = SEVEN + ONE
+
+i = int()
+
+i += TWO;   i <<= THREE
+i += TWO;   i <<= THREE
+i <<= THREE
+i += SIX;   i <<= THREE
+i += TWO;   i <<= THREE
+i += FIVE;  i <<= THREE
+i += FIVE;  i <<= THREE
+i += FOUR;  i <<= THREE
+i += THREE; i <<= THREE
+i += THREE; i <<= THREE
+i <<= THREE
+i += SIX;   i <<= THREE
+i += SEVEN; i <<= THREE
+i += FOUR;  i <<= THREE
+i += FOUR;  i <<= THREE
+i <<= THREE
+i += TWO;   i <<= THREE
+i += FIVE;  i <<= THREE
+i += SIX;   i <<= THREE
+i += SIX;   i <<= THREE
+i += SEVEN; i <<= THREE
+i += FIVE;  i <<= THREE
+i += SIX;   i <<= THREE
+i += TWO;   i <<= THREE
+i += THREE; i <<= THREE
+i += THREE; i <<= THREE
+i <<= THREE
+i += SIX;   i <<= THREE
+i += TWO;   i <<= THREE
+i <<= THREE
+i += FOUR;  i <<= THREE
+i += ONE
+
+string = i.to_bytes(SIX*TWO).decode()
+print(string)
