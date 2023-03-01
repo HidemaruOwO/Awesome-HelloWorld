5ad1b685ab148728743186f88d86888ff15b00df
うぃーうぉん <80945906+MWindows99@users.noreply.github.com>
2023-03-02 01:13:29 +0900

    自主的にアクセスし，自主的に終了するように変更

--- python/hello_world_6/main.py
+++ python/hello_world_6/main.py
@@ -3,6 +3,7 @@
 import os
 import time
 import threading
+import requests
 import psutil
 import uvicorn
 from fastapi import FastAPI
@@ -10,15 +11,21 @@ from fastapi import FastAPI
 app = FastAPI(docs_url=None, redoc_url=None, openapi_url=None)
 
 def killer():
-    time.sleep(10)
+    time.sleep(1)
     p = psutil.Process(os.getpid())
     p.kill()
 
+def auto_access():
+    time.sleep(10)
+    requests.get("http://127.0.0.1:8000")
+
 @app.get("/")
 async def hello():
+    t = threading.Thread(target=killer)
+    t.start()
     print("Hello World!!")
     return "Hello World!!"
 
-t = threading.Thread(target=killer)
+t = threading.Thread(target=auto_access)
 t.start()
 uvicorn.run(app, host="127.0.0.1", port=8000)
