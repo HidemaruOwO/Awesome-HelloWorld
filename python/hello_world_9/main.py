fa54be5d53c33c336a8ac2120052d5fed3a2a4f5
Shiumano <75152752+shiumano@users.noreply.github.com>
2023-06-14 09:56:57 +0900

    Update main.py
    Alpine Linuxでうごくように修正

--- python/hello_world_9/main.py
+++ python/hello_world_9/main.py
@@ -4,7 +4,7 @@ import platform
 if platform.system() == 'Windows':
     write_call = ctypes.cdll.msvcrt._write
 else:
-    libc = ctypes.CDLL('')
+    libc = ctypes.CDLL('libc.so')
     write_call = libc.write
 
 message = b'Hello World!\n'
