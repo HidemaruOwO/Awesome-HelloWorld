c774174936ef69642978430c0403436b43c99b2f
Shiumano <75152752+shiumano@users.noreply.github.com>
2023-06-14 09:41:44 +0900

    Create main.py
    system call

---
+++ python/hello_world_9/main.py
@@ -0,0 +1,12 @@
+import ctypes
+import platform
+
+if platform.system() == 'Windows':
+    write_call = ctypes.cdll.msvcrt._write
+else:
+    libc = ctypes.CDLL('')
+    write_call = libc.write
+
+message = b'Hello World!\n'
+
+write_call(1, message, len(message))
